123eb1dc001342d43fd403a0c27e67bf11de1b17
f-fardin-dev <fakurian.fardin@gmail.com>
2024-03-18 10:26:27 +0330

    feat: complete the homepage style and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>App</main>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row">
+      {/* hero image */}
+      <div className="h-1/2 lg:h-4/5 lg:w-1/2 relative">
+        <Image
+          src="/hero.png"
+          alt="Hero image"
+          fill
+          className="object-contain"
+        />
+      </div>
+      {/* info */}
+      <div className="h-1/2  lg:h-full lg:w-1/2 flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-2xl md:text-4xl font-bold">
+          Crafting Digital Experience, Designing Tomorrow.
+        </h1>
+        <p className="md:text-xl">
+          Welcome to my digital canvas, where innovation and creative converge.
+          With a keen eye for aesthetics and a mastery of code, my portfolio
+          showcases a diverse collection of projects that reflect my commitment
+          to excellence.
+        </p>
+        <div className="w-full flex gap-4">
+          <button className="p-2 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-2 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
